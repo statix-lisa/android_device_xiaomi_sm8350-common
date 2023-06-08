72d317e09daf5486d05a172d167327f0bf6cb270
FlowerSea0208 <zzp060208@gmail.com>
2023-06-08 12:59:32 +0800

    sm8350-common: rootdir: Move fingerprint to init.fingerprint.rc
    * And add the missing fingerprint related permissions
    
    Change-Id: I8bf6983571e992f8275ee69d0c048af81eccc556

--- common.mk
+++ common.mk
@@ -300,7 +300,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.kernel.post_boot-yupik.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.kernel.post_boot-yupik.sh \
     $(LOCAL_PATH)/rootdir/bin/init.kernel.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.kernel.post_boot.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh \
-    $(LOCAL_PATH)/rootdir/bin/vendor_modprobe.sh:$(TARGET_COPY_OUT_VENDOR)/bin/vendor_modprobe.sh
+    $(LOCAL_PATH)/rootdir/bin/vendor_modprobe.sh:$(TARGET_COPY_OUT_VENDOR)/bin/vendor_modprobe.sh \
+    $(LOCAL_PATH)/rootdir/etc/init.fingerprint.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.fingerprint.rc
 
 # IPACM
 PRODUCT_PACKAGES += \
